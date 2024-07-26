665ce810f586c09d145858bfbcaf5be64fac8742
adarsh <adarshsomanasrt@gmail.com>
2024-07-26 22:27:53 +0530

    booking routes added

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { carBooking } from '../controllers/bookingController.js'
+import { carBooking, carBookingById, getAllCarBooking } from '../controllers/bookingController.js'
 
 const router = express.Router()
 
-router.post('/booking', carBooking)
-export default router;
+router.post('/booking', carBooking);
+router.get('/bookingById/:id', carBookingById);
+router.get('/allBooking', getAllCarBooking);
+export default router;   
