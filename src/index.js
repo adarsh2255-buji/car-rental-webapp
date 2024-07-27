512072d44fde232e19f9434db614d8355e3cf77f
adarsh <adarshsomanasrt@gmail.com>
2024-07-27 23:08:11 +0530

    admin route added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import connectDb from './config/db.js';
 import userRouter from './routes/userRoutes.js';
 import carRouter from './routes/carRoutes.js'
 import bookingRouter from './routes/bookingRoutes.js'
+import adminRouter from './routes/adminRoutes.js'
 import cookieParser from 'cookie-parser';
 dotenv.config();
 const port = process.env.PORT || 3000;
@@ -17,6 +18,7 @@ app.use(cookieParser())
 app.use(userRouter)
 app.use(carRouter)
 app.use(bookingRouter)
+app.use(adminRouter)
 app.get('/', (req, res) => {
   res.send(`Hello world`);
 });
