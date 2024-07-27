03e31694e708c5f397a7e215ae1cc3188f8d4b85
adarsh <adarshsomanasrt@gmail.com>
2024-07-27 23:10:03 +0530

    updated userModel

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,29 +1,13 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-    username : {
-        type : String,
-        required : true,
-        unique : true
-    },
-    email : {
-        type : String,
-        required : true,
-        unique : true
-    },
-    password : {
-        type : String,
-        required : true
-    },
-    phone : {
-        type : String,
-        required : true
-    },
-    address : {
-        type : String,
-        required : true
-    }
-}, {timestamps: true});
+    username : { type : String, required : true, unique : true },
+    email : { type : String, required : true, unique : true },
+    password : { type : String, required : true },
+    phone : { type : String, required : true },
+    address : { type : String, required : true },
+    isAdmin : { type : Boolean, default : false }
+}, { timestamps: true } );
 
 const User = mongoose.model("User", userSchema);
 
