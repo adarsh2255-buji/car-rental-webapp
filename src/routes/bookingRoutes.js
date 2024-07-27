c8fdb4a0dd150f4c62a8fcd1365231418deebc1d
adarsh <adarshsomanasrt@gmail.com>
2024-07-27 23:10:56 +0530

    cancellation updated

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { carBooking, carBookingById, getAllCarBooking } from '../controllers/bookingController.js'
+import { cancelBooking, carBooking, carBookingById, getAllCarBooking } from '../controllers/bookingController.js'
+import protect from '../middlewares/authMiddleware.js';
 
 const router = express.Router()
 
-router.post('/booking', carBooking);
-router.get('/bookingById/:id', carBookingById);
-router.get('/allBooking', getAllCarBooking);
+router.post('/booking',protect, carBooking);
+router.get('/bookingById/:id', protect, carBookingById);
+router.get('/allBooking', protect, getAllCarBooking);
+router.put('/cancel/:bookingId',protect, cancelBooking);
 export default router;   
